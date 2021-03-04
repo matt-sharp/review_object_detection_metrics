87b516345ab13380e509252c5ce553e4cb1e6c9a
Matthew Sharp <matthew.sharp@gitlab-ad.ddap-dev.ice.mod.gov.uk>
2021-03-04 16:37:41 +0000

    Add tabs to GUI interface
    Update the GUI interface so that it displays the ground truth and
    detection sections as tabs.  This allows for the entire GUI to be
    viewed on the screen.  The previous version could not be re-sized
    to fit the screen.

--- src/ui/run_ui.py
+++ src/ui/run_ui.py
@@ -7,7 +7,8 @@ from PyQt5.QtWidgets import QFileDialog, QMainWindow, QMessageBox
 from src.evaluators.coco_evaluator import get_coco_summary
 from src.evaluators.pascal_voc_evaluator import (get_pascalvoc_metrics, plot_precision_recall_curve)
 from src.ui.details import Details_Dialog
-from src.ui.main_ui import Ui_Dialog as Main_UI
+# from src.ui.main_ui import Ui_Dialog as Main_UI
+from src.ui.main_ui_tabs import Ui_Dialog as Main_UI
 from src.ui.results import Results_Dialog
 from src.ui.splash import Splash_Dialog
 from src.utils.enumerators import BBFormat, BBType, CoordinatesType
